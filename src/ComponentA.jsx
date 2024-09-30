63fa5444a1b9662924fc632a123229b993a576e3
kishore 01 <vkkishore886@gmail.com>
2024-09-30 17:24:21 +0530

    Update ComponentA.jsx

--- src/ComponentA.jsx
+++ src/ComponentA.jsx
@@ -9,7 +9,7 @@ export const Usercontext = createContext();
 function ComponentA () {
     return (
         <div className="box">
-            <h2>Hello Guest...!</h2>
+            <h2>Hello Guest.!</h2>
             <Usercontext.Provider>
                 <ComponentB />
             </Usercontext.Provider>
